c7e697bc621bff2b072abef86d06f9bdc86a1909
Chu-Cheng Lin <chuchenglin@cmu.edu>
2015-01-24 01:52:27 -0500

    slightly more efficient

--- hw1/model1.py
+++ hw1/model1.py
@@ -5,7 +5,7 @@ from io import open
 import random
 
 
-def init_align(e_sentences, g_sentences, e_vocab, g_vocab):
+def init_align(numbered_e_sentences, numbered_g_sentences):
     # counts of alignments g -> *
     g_counts = defaultdict(int)
 
@@ -13,18 +13,18 @@ def init_align(e_sentences, g_sentences, e_vocab, g_vocab):
     g_e_counts = defaultdict(lambda: defaultdict(int))
 
     alignments = []
-    for g_sen, e_sen in zip(g_sentences,e_sentences):
+    for g_sen, e_sen in zip(numbered_g_sentences,numbered_e_sentences):
         alignment = []
         for e in e_sen:
             a = random.randint(0, len(g_sen))
             alignment.append(a)
             if a == 0:
                 g_counts[0] += 1
-                g_e_counts[0][e_vocab[e]] += 1
+                g_e_counts[0][e] += 1
             else:
                 g_idx = a - 1
-                g_counts[g_vocab[g_sen[g_idx]]] += 1
-                g_e_counts[g_vocab[g_sen[g_idx]]][e_vocab[e]] += 1
+                g_counts[g_sen[g_idx]] += 1
+                g_e_counts[g_sen[g_idx]][e] += 1
         alignments.append(alignment)
     return (alignments, g_counts, g_e_counts)
 
@@ -40,43 +40,47 @@ def sample_from(table):
     # unlikely
     return len(table)-1
 
-def sample(alignment, g_sen, e_sen, g_counts, g_e_counts, g_vocab, e_vocab):
+def sample(alignment, g_sen, e_sen, g_counts, g_e_counts, e_vocab_size):
 
     theta = 1
     for a_idx, (a,e) in enumerate(zip(alignment, e_sen)):
         # first remove count
         if a == 0:
             g_counts[0] -= 1
-            g_e_counts[0][e_vocab[e]] -= 1
+            g_e_counts[0][e] -= 1
         else:
             g_idx = a - 1
-            g_counts[g_vocab[g_sen[g_idx]]] -= 1
-            g_e_counts[g_vocab[g_sen[g_idx]]][e_vocab[e]] -= 1
+            g_counts[g_sen[g_idx]] -= 1
+            g_e_counts[g_sen[g_idx]][e] -= 1
         prob_table = []
-        prob_table.append((g_e_counts[0][e_vocab[e]] + theta) * 1. / (g_counts[0] + theta * (len(e_vocab)-1) ))
+        prob_table.append((g_e_counts[0][e] + theta) * 1. / (g_counts[0] + theta * (e_vocab_size-1) ))
         for g in g_sen:
-            idx = g_vocab[g]
-            prob_g = (g_e_counts[idx][e_vocab[e]] + theta) * 1. / (g_counts[idx] + theta * (len(e_vocab)-1) )
+            idx = g
+            prob_g = (g_e_counts[idx][e] + theta) * 1. / (g_counts[idx] + theta * (e_vocab_size-1) )
             prob_table.append(prob_g)
         sampled = sample_from(prob_table)
         alignment[a_idx] = sampled
         if sampled == 0:
             g_counts[0] += 1
-            g_e_counts[0][e_vocab[e]] += 1
+            g_e_counts[0][e] += 1
         else:
             g_idx = sampled - 1
-            g_counts[g_vocab[g_sen[g_idx]]] += 1
-            g_e_counts[g_vocab[g_sen[g_idx]]][e_vocab[e]] += 1
+            g_counts[g_sen[g_idx]] += 1
+            g_e_counts[g_sen[g_idx]][e] += 1
     return
 
 
 def create_vocab(sentences):
     all_elems = dict()
+    to_return = []
     for s in sentences:
+        to_append = []
         for w in s:
             if w not in all_elems:
                 all_elems[w] = len(all_elems)+1
-    return all_elems
+            to_append.append(all_elems[w])
+        to_return.append(to_append)
+    return all_elems, to_return
 
 def read_aligned(fname):
     f_sentences = []
@@ -92,15 +96,30 @@ def read_aligned(fname):
     return (f_sentences, e_sentences)
 
 
+def record(alignments):
+    print 'dummy -- should be recording'
+    return
+
+
 def main():
     import sys
     (f,e) = read_aligned(sys.argv[1])
-    (f_vocab, e_vocab) = (create_vocab(f), create_vocab(e))
+    f_vocab, numbered_f = create_vocab(f)
+    e_vocab, numbered_e = create_vocab(e)
     print len(f_vocab), len(e_vocab)
-    (alignments, f_counts, f_e_counts) = init_align(e,f,e_vocab,f_vocab)
-    for sen_idx in range(len(alignments)):
-        print 'idx: {}'.format(sen_idx)
-        sample(alignments[sen_idx], f[sen_idx], e[sen_idx], f_counts, f_e_counts, f_vocab, e_vocab)
+    (alignments, f_counts, f_e_counts) = init_align(numbered_e,numbered_f)
+
+    # epochs
+    num_epochs = 5
+    burnins = 1
+    for epoch in range(num_epochs):
+        print 'epoch {}:'.format(epoch)
+        shuffled = range(len(alignments))
+        random.shuffle(shuffled)
+        for sen_idx in shuffled:
+            sample(alignments[sen_idx], numbered_f[sen_idx], numbered_e[sen_idx], f_counts, f_e_counts, len(e_vocab))
+        if epoch + 1 > burnins:
+            record(alignments)
     return
 
 if __name__ == '__main__':
